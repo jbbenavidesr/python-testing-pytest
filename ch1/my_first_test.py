f800626328cc7fc3f22d0d21b2921dacac9169a5
Juan Bernardo Benavides Rubio <jbbenavidesr@gmail.com>
2022-11-28 21:41:39 -0500

    :sparkles: Add sample failing tests in ch1

---
+++ ch1/my_first_test.py
@@ -0,0 +1,10 @@
+def test_number_in_list():
+    assert 1 in [2, 3, 4]
+
+def test_a_less_than_b():
+    a = 10
+    b = 8
+    assert a < b
+
+def test_substring_not_in_string():
+    assert 'fizz' not in 'fizzbuzz'
